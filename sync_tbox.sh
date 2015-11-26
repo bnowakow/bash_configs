1cbe3d8782809c9d08aff1bb56531a8ca2d4c2c6
Bartek Nowakowski <bartek@bnowakowski.pl>
2015-11-26 12:02:08 +0100

    Remove files from server after sync Remove debug lines.

--- sync_tbox.sh
+++ sync_tbox.sh
@@ -6,12 +6,6 @@ REMOTE_SSH="kszczep@tbox"
 PID_FILE="/var/run/sync_tbox.pid"
 
 declare -a FILES=('Castle' 'Homeland' 'How.to.Get.Away.with.Murder' 'Fargo' 'Last.Week.Tonight');
-# declare -a FILES=('Castle' 'Homeland');
-# declare -a FILES=('Castle');
-
-
-# debug 
-echo `date` >> /var/log/sync_tbox.log
 
 if [ -f $PID_FILE ]; then
     echo "Progam already runs in PID=`cat $PID_FILE`, exiting.";
@@ -21,8 +15,7 @@ echo $! > $PID_FILE;
 
 for file in "${FILES[@]}"; do
 	echo $file;
-	# --include="*/"
-	rsync -r --partial --progress --rsh=ssh --include="*/" --include="*$file*" --include="*$file*/" --include="*/*$file*" --exclude="*" $REMOTE_SSH:"$REMOTE_DIR" $LOCAL_DIR;	
+	rsync -r --partial --progress --rsh=ssh --include="*/" --include="*$file*" --include="*$file*/" --include="*/*$file*" --exclude="*" --remove-source-files $REMOTE_SSH:"$REMOTE_DIR" $LOCAL_DIR;
 done
 
 rm $PID_FILE;
