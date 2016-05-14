3d11c8c7630396558e43ade5eb82a359eea16140
Bartek Nowakowski <bartek@bnowakowski.pl>
2016-05-14 17:12:22 +0200

    small fixes

--- ovh_backup_download.sh
+++ ovh_backup_download.sh
@@ -3,9 +3,10 @@
 host=ovh.bnowakowski.pl
 remote_dir=/backup
 remote_file_name="sup*"
-local_dir=/Users/sup/code/ovh/backup
+local_dir="/Users/sup/code/ovh/backup"
 number_of_backpus_to_keep=3
 
-rsync --partial --progress --rsh=ssh -r --remove-source-files $host:\"$remote_dir/$remote_file_name\" "$local_dir"
+remote_path="$remote_dir/$remote_file_name";
+rsync --partial --progress --rsh=ssh -r --remove-source-files $host:$remote_path $local_dir;
 let number_of_backpus_to_keep=$number_of_backpus_to_keep+1;
-ls -d -1t $local_dir/* | tail -n +$number_of_backpus_to_keep | xargs rm
+ls -d -1t $local_dir/* | tail -n +$number_of_backpus_to_keep | xargs rm;
