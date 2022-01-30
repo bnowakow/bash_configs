98ea2bd7ab28645df816189b6fec116d2d3884af
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-01-30 14:21:27 +0100

    moving ovh_backup_download.sh to margok nas. backuping all users, iterating through them when deleting old backups. checking if there's enought backups to delete them

--- ovh_backup_download.sh
+++ ovh_backup_download.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-host=ovh.bnowakowski.pl
-remote_dir=/backup
-local_dir="/Users/sup/code/ovh/backup"
-remote_file_name="sup*"
+remote_host=ovh.bnowakowski.pl
+remote_dir="/backup"
 remote_file_name="*.tar"
+
+local_dir="/mnt/MargokPool/archive/Documents/ovh"
+
 number_of_backpus_to_keep=10
 
 mkdir -p "$local_dir"
 remote_path="$remote_dir/$remote_file_name";
-rsync --partial --progress --rsh=ssh -r --remove-source-files $host:$remote_path $local_dir;
+rsync --partial --progress --rsh=ssh -r --remove-source-files $remove_host:$remote_path $local_dir;
+
 let number_of_backpus_to_keep=$number_of_backpus_to_keep+1;
-ls -d -1t $local_dir/* | tail -n +$number_of_backpus_to_keep | xargs rm;
+for user in $( ls -1 $local_dir | sed 's/\..*//' | sort | uniq ); do
+    echo $user;
+    number_of_backup_files=$(ls -d -1t $local_dir/$user* | wc -l);
+    if [ $number_of_backup_files -gt $number_of_backpus_to_keep ]; then
+        ls -d -1t $local_dir/$user* | tail -n +$number_of_backpus_to_keep | xargs rm;
+    fi
+done
