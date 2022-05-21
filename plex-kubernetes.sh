de1780d1b5adeed4863aa80d8fda4e6c61121160
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-05-21 20:17:54 +0200

    add additional commands to plex-kubernetes.sh

--- plex-kubernetes.sh
+++ plex-kubernetes.sh
@@ -7,18 +7,40 @@ pod_params=$(sudo k3s kubectl get pods --all-namespaces -o wide | grep $name)
 pod_namespace=$(echo $pod_params | awk '{print $1}')
 pod_name=$(echo $pod_params | awk '{print $2}')
 pod_ip=$(sudo k3s kubectl describe pod $pod_name -n $pod_namespace | egrep '^IP:' | awk '{print $2}')
-pod_health_check_script_patch=$(sudo k3s kubectl describe pod $pod_name -n $pod_namespace | grep Liveness | awk '{print $3}' | sed 's/^.//' | sed 's/.$//')
+pod_health_check_script_path=$(sudo k3s kubectl describe pod $pod_name -n $pod_namespace | grep Liveness | awk '{print $3}' | sed 's/^.//' | sed 's/.$//')
 
-# describe pod
+#echo describe pod
 #sudo k3s kubectl describe pod $pod_name -n $pod_namespace | less
+#echo
 
-# console inside pod
+#echo console inside pod
 #sudo k3s kubectl exec -n $pod_namespace -it $pod_name -- bash
 
-# check health script
+#echo health check script
 #sudo k3s kubectl exec -n $pod_namespace -it $pod_name -- cat $pod_health_check_script_patch
+#echo
 
-# health check script
-sudo k3s kubectl exec -n $pod_namespace -it $pod_name -- bash -c "$pod_health_check_script_patch; echo $?"
+#echo run health check script inside pod
+#sudo k3s kubectl exec -n $pod_namespace -it $pod_name -- bash -c "time $pod_health_check_script_path; echo $?"
+#echo
 
+echo manual health check from host
+time curl -ksf http://$pod_ip:32400/identity; echo $?
+echo
+
+echo show system events
+sudo k3s kubectl get events
+echo
+
+echo show namespace events
+sudo k3s kubectl get events --namespace=$pod_namespace
+echo
+
+#echo current logs
+#sudo k3s kubectl logs $pod_name --namespace=$pod_namespace
+#echo
+
+#echo previous logs
+#sudo k3s kubectl logs $pod_name --namespace=$pod_namespace --previous
+#echo
 
