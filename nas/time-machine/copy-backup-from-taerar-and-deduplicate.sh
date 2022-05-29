64cd48f4b734212b93f9e5ea0cfde25affb03d71
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-05-29 16:02:35 +0200

    check if you can read time-machine directory after mount. small fixes

---
+++ nas/time-machine/copy-backup-from-taerar-and-deduplicate.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source lib/mount_apfs.sh
+
+
--- nas/time-machine/lib/mount_apfs.sh
+++ nas/time-machine/lib/mount_apfs.sh
@@ -6,6 +6,9 @@ column_number=6;
 disk_grep="Elements";
 column_number=7;
 
+mount_path=/media/Taerar7
+sudo mkdir -p $mount_path
+
 disk_device=$(lsscsi | grep $disk_grep | awk '{print $'$column_number'}')
 partition_number=2
 
@@ -13,9 +16,9 @@ partition_device=$disk_device$partition_number
 echo $partition_device
 
 # https://linuxnewbieguide.org/how-to-mount-macos-apfs-disk-volumes-in-linux/
-#sudo apfs-fuse -o allow_other $partition_device /media/tm #ro
+#sudo apfs-fuse -o allow_other $partition_device $mount_path #ro
 
 # https://www.reddit.com/r/datarecovery/comments/kkamgy/apfs_rw_support_for_linux/
-sudo fsapfsmount -f 1 $partition_device /media/Taerar7 #rw
+sudo fsapfsmount -f 1 $partition_device $mount_path #rw
 
-sudo ls /media/Taerar7
+sudo find $mount_path -maxdepth 1 -name "*.backup" | sudo xargs -I{} ls -l {}/Data
