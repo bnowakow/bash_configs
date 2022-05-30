e700f8eed454dd6145d951450bfb7fa0cdf5d898
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-05-30 07:06:32 +0200

    support for both apfs-fuse (ro) and fsapfsmount (rw) in mount_apfs.sh

--- nas/time-machine/copy-backup-from-taerar-and-deduplicate.sh
+++ nas/time-machine/copy-backup-from-taerar-and-deduplicate.sh
@@ -3,9 +3,7 @@
 # todo check if u can set mount_path in main script and use it in sourced one
 
 source lib/mount_apfs.sh
-sudo ls -la $mount_path
+sudo ls -la $mount_path_tm
 
-backup_path=/mnt/MargokPool/archive/TimeMachine
-
-sudo rsync -a --no-links $mount_path $backup_path 2>&1 | grep -v "skipping non-regular file"
+sudo rsync -a --no-links $mount_path_tm $backup_path 2>&1 | grep -v "skipping non-regular file"
 
--- nas/time-machine/lib/mount_apfs.sh
+++ nas/time-machine/lib/mount_apfs.sh
@@ -16,9 +16,15 @@ partition_device=$disk_device$partition_number
 echo $partition_device
 
 # https://linuxnewbieguide.org/how-to-mount-macos-apfs-disk-volumes-in-linux/
-#sudo apfs-fuse -o allow_other $partition_device $mount_path #ro
+sudo apfs-fuse -o allow_other $partition_device $mount_path #ro
+ls -la $mount_path
+sudo find $mount_path -maxdepth 3 -name "Data" | sudo xargs -I{} ls -l {}
+mount_path_tm=$mount_path/root/*
+backup_path=/mnt/MargokPool/archive/TimeMachine/Taerar7/
 
 # https://www.reddit.com/r/datarecovery/comments/kkamgy/apfs_rw_support_for_linux/
-sudo fsapfsmount -f 1 $partition_device $mount_path #rw
+#sudo fsapfsmount -f 1 $partition_device $mount_path #rw
+#sudo find $mount_path -maxdepth 1 -name "*.backup" | sudo xargs -I{} ls -l {}/Data
+#mount_path_tm=$mount_path
+#backup_path=/mnt/MargokPool/archive/TimeMachine
 
-sudo find $mount_path -maxdepth 1 -name "*.backup" | sudo xargs -I{} ls -l {}/Data
