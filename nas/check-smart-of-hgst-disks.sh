df85088d812a91c34252c94865d4bacd5b5a1221
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-05-30 17:13:53 +0200

    add check-smart-of-hgst-disks.sh. change rsync params in copy-backup-from-taerar-and-deduplicate.sh

---
+++ nas/check-smart-of-hgst-disks.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+for disk in /dev/sd*; do
+    # process only disks not partitions
+    echo $disk | grep -v [0-9] > /dev/null;
+    if [ $? == 1 ]; then
+        continue;
+    fi;
+    
+    # process only HGST disks
+    sudo smartctl -a $disk | grep "Device Model" | grep HGST > /dev/null
+    if [ $? == 1 ]; then
+        continue;
+    fi;
+
+
+    sudo hddtemp $disk;
+    
+    #echo $disk;
+    #sudo smartctl -a $disk | grep "Device Model"
+    
+    sudo smartctl -a $disk | grep Power_On_Hours
+    sudo smartctl -a $disk | grep UDMA_CRC_Error_Count
+    sudo smartctl -a $disk | grep self-assessment
+    #sudo smartctl -a $disk | grep Celsius | sed 's/[^-]*//'
+    
+    echo
+done
--- nas/time-machine/copy-backup-from-taerar-and-deduplicate.sh
+++ nas/time-machine/copy-backup-from-taerar-and-deduplicate.sh
@@ -5,5 +5,9 @@
 source lib/mount_apfs.sh
 sudo ls -la $mount_path_tm
 
-sudo rsync -a --no-links $mount_path_tm $backup_path 2>&1 | grep -v "skipping non-regular file"
+sudo rsync -a $mount_path_tm $backup_path 2>&1 | grep -v "skipping non-regular file"
+
+sudo umount $mount_path
+
+
 
