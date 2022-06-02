68765fac57e06f46209cd6529d292d6ffca51d3a
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-06-02 15:08:45 +0200

    add deduplicate with whole nas

--- nas/time-machine/copy-backup-from-taerar-and-deduplicate.sh
+++ nas/time-machine/copy-backup-from-taerar-and-deduplicate.sh
@@ -1,13 +1,22 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # todo check if u can set mount_path in main script and use it in sourced one
-
+# todo check return codes of mounting, maybe rsync
 source lib/mount_apfs.sh
 sudo ls -la $mount_path_tm
 
-sudo rsync -a $mount_path_tm $backup_path 2>&1 | grep -v "skipping non-regular file"
+sudo rsync -a $mount_path_tm $backup_path_tm
 
 sudo umount $mount_path
 
+cd $backup_path
+reversed_directories_tm=$(ls -r -d */)
+sudo jdupes --delete --noprompt -r -Z -O $reversed_directories_tm
+sudo find "$backup_path" -depth -type d -empty -delete;
 
+cd /mnt/MargokPool/home/sup/code/docker-jdupes-gui
+docker-compose up
+docker-compose down
+
+sudo find "$backup_path" -depth -type d -empty -delete;
 
--- nas/time-machine/lib/mount_apfs.sh
+++ nas/time-machine/lib/mount_apfs.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-disk_grep="2105";
-column_number=6;
+#disk_grep="2105";
+#column_number=6;
 
 disk_grep="Elements";
 column_number=7;
 
 mount_path=/media/Taerar7
 sudo mkdir -p $mount_path
+backup_path=/mnt/MargokPool/archive/TimeMachine
 
 disk_device=$(lsscsi | grep $disk_grep | awk '{print $'$column_number'}')
 partition_number=2
@@ -20,11 +21,10 @@ sudo apfs-fuse -o allow_other $partition_device $mount_path #ro
 ls -la $mount_path
 sudo find $mount_path -maxdepth 3 -name "Data" | sudo xargs -I{} ls -l {}
 mount_path_tm=$mount_path/root/*
-backup_path=/mnt/MargokPool/archive/TimeMachine/Taerar7/
+backup_path_tm=/mnt/MargokPool/archive/TimeMachine/Taerar7/
 
 # https://www.reddit.com/r/datarecovery/comments/kkamgy/apfs_rw_support_for_linux/
 #sudo fsapfsmount -f 1 $partition_device $mount_path #rw
 #sudo find $mount_path -maxdepth 1 -name "*.backup" | sudo xargs -I{} ls -l {}/Data
 #mount_path_tm=$mount_path
-#backup_path=/mnt/MargokPool/archive/TimeMachine
-
+#backup_path_tm=$backup_path
