3a843bbc0b68fb817c1bb44cc5fc88c1d2bcb38c
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-06-30 13:41:28 +0200

    wip on ssh keys that are used with cron scripts

--- nas/provision.sh
+++ nas/provision.sh
@@ -13,12 +13,6 @@ sudo apt-cache madison linux-headers-truenas-amd64
 sudo apt install -y linux-headers-truenas-amd64
 sudo apt install -y virtualbox-6.1 dkms
 
-sudo apt-get install software-properties-common dirmngr apt-transport-https -y 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C4A05888A1C4FA02E1566F859F2A29A569653940 
-sudo add-apt-repository "deb http://kryptco.github.io/deb kryptco main" # non-Kali Linux only 
-sudo apt-get update 
-sudo apt-get install kr -y 
-
 # https://computingforgeeks.com/how-to-install-latest-docker-compose-on-linux/
 curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url  | grep docker-compose-linux-x86_64 | cut -d '"' -f 4 | wget -qi -
 chmod +x docker-compose-linux-x86_64
@@ -26,11 +20,23 @@ sudo mv docker-compose-linux-x86_64 /usr/local/bin/docker-compose
 
 chsh -s $(which zsh)
 
-if ! crontab -l | grep ovh_backup_download; then
-    { crontab -l; echo '02 05 * * * /mnt/MargokPool/home/sup/code/bash_configs/proxmox_backup_download.sh'; } | crontab -
-    { crontab -l; echo '02 15 * * * /mnt/MargokPool/home/sup/code/bash_configs/ovh_backup_download.sh'; } | crontab -
-fi
-
 # https://linuxnewbieguide.org/how-to-mount-macos-apfs-disk-volumes-in-linux/
 sudo cp /mnt/MargokPool/home/sup/code/apfs-fuse/build/apfs-* /usr/local/bin
+
+if ! crontab -l | grep ovh_backup_download; then
+    { crontab -l; echo '02 05 * * * /mnt/MargokPool/home/sup/code/bash_configs/nas/cron/proxmox_backup_download.sh'; } | crontab -
+    { crontab -l; echo '02 15 * * * /mnt/MargokPool/home/sup/code/bash_configs/nas/cron/ovh_backup_download.sh'; } | crontab -
+fi
+
+# user interactive - generate and copy ssh keys
+ssh-keygen
+ssh-copy-id root@192.168.1.56
+
+sudo apt-get install software-properties-common dirmngr apt-transport-https -y
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C4A05888A1C4FA02E1566F859F2A29A569653940
+sudo add-apt-repository "deb http://kryptco.github.io/deb kryptco main" # non-Kali Linux only
+sudo apt-get update
+sudo apt-get install kr -y
+
+ssh-copy-id -i /mnt/MargokPool/home/sup/.ssh/id_rsa.pub ovh.bnowakowski.pl
 
