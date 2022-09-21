824821b4f85e059f038fbf09f3c1ed1a42f143b1
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-09-21 15:54:47 +0200

    fix error in number-of-days-since-last-backup.sh, no sorting by time so we didn't took last file

--- nas/zabbix/number-of-days-since-last-backup.sh
+++ nas/zabbix/number-of-days-since-last-backup.sh
@@ -4,7 +4,7 @@ backup_dir="${1:-ovh}"
 
 backup_prefix_dir="/mnt/MargokPool/archive/Documents";
 
-last_backup_date=$(ls --full-time $backup_prefix_dir/$backup_dir/*tar* | tail -1 | awk '{print $6}')
+last_backup_date=$(ls --full-time -t $backup_prefix_dir/$backup_dir/*tar* | head -1 | awk '{print $6}')
 curent_date=$(date +%Y-%m-%d)
 
 days_diff=$(( (`date -d $curent_date +%s` - `date -d $last_backup_date +%s`) / (24*3600) ));
