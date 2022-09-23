0abef2b0f712590bce12ddbf0283007d5d10f54a
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-09-23 20:14:56 +0200

    add is-crashplan-on-vagrant-running.sh

---
+++ nas/zabbix/is-crashplan-on-vagrant-running.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo su sup -c 'cd /mnt/MargokPool/home/sup/code/crashplan-docker; \
+    vagrant ssh -c "if docker exec crashplan /usr/local/crashplan/bin/service.sh status | grep running > /dev/null; then echo true; else echo false; fi" 2>/dev/null'
