b5c007dda0cf62bd6b5653a520f248d9b39b7d55
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-09-28 21:26:41 +0200

    fixes for is-helm-image-up-to-date.sh to work with sudo

--- nas/zabbix/is-helm-image-up-to-date.sh
+++ nas/zabbix/is-helm-image-up-to-date.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#helm repo add TrueCharts https://charts.truecharts.org
+helm repo add TrueCharts https://charts.truecharts.org > /dev/null
 helm repo update > /dev/null
 
 #helm pull TrueCharts/duckdns
@@ -9,9 +9,9 @@ name="${1:-duckdns}"
 version_current=$(helm search repo TrueCharts/$name --versions | head -2 | tail -1 | awk '{print $2}')
 
 # https://github.com/k3s-io/k3s/issues/1126
-if sudo helm ls --all-namespaces --kubeconfig /etc/rancher/k3s/k3s.yaml | grep $name | awk '{print $9}' | sed 's/.*-//' | grep $version_current; then
+if sudo /bin/helm ls --all-namespaces --kubeconfig /etc/rancher/k3s/k3s.yaml | grep $name | awk '{print $9}' | sed 's/.*-//' | grep $version_current; then
     echo true;
 else
-    echo false,$(sudo helm ls --all-namespaces --kubeconfig /etc/rancher/k3s/k3s.yaml | grep $name | awk '{print $9}'),$version_current;
+    echo false,$(sudo /bin/helm ls --all-namespaces --kubeconfig /etc/rancher/k3s/k3s.yaml | grep $name | awk '{print $9}'),$version_current;
 fi
 
