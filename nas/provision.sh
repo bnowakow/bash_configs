7cfb690609fa4e42da62614237a12a6366d9bb07
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-10-04 19:17:51 +0200

    add sudo for zabbix

--- nas/provision.sh
+++ nas/provision.sh
@@ -66,6 +66,7 @@ sudo mkdir -p /var/lib/zabbix/
 sudo chown zabbix:zabbix /var/lib/zabbix
 sudo ./bash_configs/zabbix/update-zabbix-metadata.sh
 # TODO add sudoers for zabbix
+cat zabbix-sudoers | sudo tee -a /etc/sudoers
 # TODO add bash for zabbix account, run 
 # /etc/zabbix/zabbix_agent2.d/bash_configs/nas/zabbix/is-plex-running/1-install-depencencies.sh
 # /etc/zabbix/zabbix_agent2.d/bash_configs/nas/zabbix/is-plex-running/2-build.sh
