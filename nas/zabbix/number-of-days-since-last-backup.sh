994827cdf52b64cddd72cb75e4ddb08dca2370fb
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-10-07 22:08:22 +0200

    update backup dir

--- nas/zabbix/number-of-days-since-last-backup.sh
+++ nas/zabbix/number-of-days-since-last-backup.sh
@@ -2,7 +2,7 @@
 
 backup_dir="${1:-ovh}"
 
-backup_prefix_dir="/mnt/MargokPool/archive/Documents";
+backup_prefix_dir="/mnt/MargokPool/archive/Backups";
 
 last_backup_date=$(ls --full-time -t $backup_prefix_dir/$backup_dir/*tar* | head -1 | awk '{print $6}')
 curent_date=$(date +%Y-%m-%d)
