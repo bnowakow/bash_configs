5347a77cf74ca1e0dbc05414efa79875e2921139
root <root@margok-proxmox.home>
2022-10-22 23:13:44 +0200

    add status after update-zabbix-metadata.sh

--- zabbix/update-zabbix-metadata.sh
+++ zabbix/update-zabbix-metadata.sh
@@ -3,4 +3,5 @@
 rm /etc/zabbix/zabbix_agent2.d/zabbix*.conf
 find -L /etc/zabbix/zabbix_agent2.d -name 'zabbix*.conf' | grep -v zabbix_agent2.conf | xargs -I{} cp {} /etc/zabbix/zabbix_agent2.d/
 service zabbix-agent2 restart
+service zabbix-agent2 status
 
