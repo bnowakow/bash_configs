343c3b3c081323046b44924592f79ef3705be793
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-10-22 23:12:30 +0200

    workaround for not including zabbix_agent2.conf in ./update-zabbix-metadata.sh

--- zabbix/update-zabbix-metadata.sh
+++ zabbix/update-zabbix-metadata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm /etc/zabbix/zabbix_agent2.d/zabbix*.conf
-find -L /etc/zabbix/zabbix_agent2.d -name 'zabbix*.conf' | xargs -I{} cp {} /etc/zabbix/zabbix_agent2.d/
+find -L /etc/zabbix/zabbix_agent2.d -name 'zabbix*.conf' | grep -v zabbix_agent2.conf | xargs -I{} cp {} /etc/zabbix/zabbix_agent2.d/
 service zabbix-agent2 restart
 
