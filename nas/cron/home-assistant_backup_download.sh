01e5185e4f41f9b3e287dec34dd18be6bee09259
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-10-23 11:20:18 +0200

    update home assistant scripts since it moved from vagrant on margok to separate vm on proxmox

--- nas/cron/home-assistant_backup_download.sh
+++ nas/cron/home-assistant_backup_download.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-remote_host=localhost
+remote_host=192.168.1.67
 remote_user=root
 remote_dir="/backup"
 remote_file_name="*.tar"
-remote_port=$(cd /mnt/MargokPool/home/sup/code/bash_configs/nas/home-assistant; vagrant port --guest 22)
 
 local_dir="/mnt/MargokPool/archive/Backups/home-assistant"
 
@@ -12,7 +11,7 @@ number_of_backpus_to_keep=30
 
 mkdir -p "$local_dir"
 remote_path="$remote_dir/$remote_file_name";
-rsync --partial --progress --rsh=ssh -r --remove-source-files -e "ssh -i $HOME/.ssh/id_rsa -p $remote_port" $remote_user@$remote_host:$remote_path $local_dir;
+rsync --partial --progress --rsh=ssh -r --remove-source-files -e "ssh -i $HOME/.ssh/id_rsa" $remote_user@$remote_host:$remote_path $local_dir;
 
 number_of_backup_files=$(ls -d -1t $local_dir/* | wc -l);
 if [ $number_of_backup_files -gt $number_of_backpus_to_keep ]; then
