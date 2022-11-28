26762ed2b75ac83c4e9152393f8b8a14f143d08c
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-11-29 00:13:52 +0100

    check number of backup files in ovh/cron/etc-backup.sh

--- ovh/cron/etc-backup.sh
+++ ovh/cron/etc-backup.sh
@@ -4,7 +4,11 @@ number_of_backpus_to_keep=10
 backup_path=/home/sup/code/bash_configs/ovh/cron/etc-backup
 
 mkdir -p $backup_path
-ls -1t $backup_path/*.tar.gz | tail -n +$number_of_backpus_to_keep | xargs rm;
+
+number_of_backup_files=$(ls -d -1t $backup_path/*.tar.gz | wc -l);
+if [ $number_of_backup_files -gt $number_of_backpus_to_keep ]; then
+    ls -d -1t $backup_path/*.tar.gz | tail -n +$number_of_backpus_to_keep | xargs rm;
+fi
 
 date_stamp=$(date +"%Y-%m-%d_%H-%M")
 sudo cp -r /etc $backup_path/etc-$date_stamp
