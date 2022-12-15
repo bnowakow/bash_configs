f1602b13d6305381425a3a011dc6118153aa31b1
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-12-15 19:23:11 +0100

    add TODO to update-zabbix-metadata.sh

--- zabbix/update-zabbix-metadata.sh
+++ zabbix/update-zabbix-metadata.sh
@@ -5,3 +5,6 @@ find -L /etc/zabbix/zabbix_agent2.d -name 'zabbix*.conf' | grep -v zabbix_agent2
 service zabbix-agent2 restart
 service zabbix-agent2 status
 
+# TODO wait 5? seconds check status, if failed show
+# journalctl -u zabbix-agent2
+
