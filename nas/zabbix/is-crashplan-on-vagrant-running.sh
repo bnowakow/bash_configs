0a7e760a09704d127e26e00a622c67bd2dd58265
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2023-06-14 18:24:41 +0200

    move crashplan from nas to standalone server

--- nas/zabbix/is-crashplan-on-vagrant-running.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo /usr/bin/su sup -c 'cd /mnt/MargokPool/home/sup/code/crashplan-docker; vagrant ssh -c "if sudo /usr/local/crashplan/bin/service.sh status | grep running > /dev/null; then echo true; else echo false; fi" 2>/dev/null'
