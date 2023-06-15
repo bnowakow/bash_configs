74cd222b3b9a414e691acf8ee5bee2267e5f77a4
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2023-06-15 05:23:17 +0200

    move kibana zabbix outside of nas

--- kibana/zabbix/is-kibana-up-to-date.sh
+++ kibana/zabbix/is-kibana-up-to-date.sh
@@ -2,7 +2,7 @@
 
 current_version=$(curl https://www.elastic.co/downloads/elasticsearch 2> /dev/null | grep Version | head -1 | sed 's/.*Version:\ <\/strong>//' | sed 's/<.*//');
 # TODO remove \r from end of local_version
-local_version=$(sudo /usr/bin/su sup -c 'cd /mnt/MargokPool/home/sup/code/bash_configs/nas/zabbix/sie-pomaga; vagrant ssh -c "docker ps | grep elasticsearch | sed s/.*elasticsearch:// | sed s/\ .*$//" 2>/dev/null');
+local_version=$(docker ps | grep elasticsearch | sed s/.*elasticsearch:// | sed s/\ .*$//);
 
 if [ "$current_version" = "$local_version" ]; then
     echo "true";
