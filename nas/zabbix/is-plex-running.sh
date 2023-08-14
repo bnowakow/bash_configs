b3ee97076cf9507a204c19106bcdd6eb491f4529
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2023-08-14 12:40:23 -0700

    don't display errors in is-plex-running.sh for https://github.com/urllib3/urllib3/issues/3020

--- nas/zabbix/is-plex-running.sh
+++ nas/zabbix/is-plex-running.sh
@@ -2,6 +2,7 @@
 
 cd /etc/zabbix/zabbix_agent2.d/bash_configs/nas/zabbix
 cd is-plex-running
-./3-run.sh
+# NotOpenSSLWarning: urllib3 v2.0 only supports OpenSSL 1.1.1+, currently the 'ssl' module is compiled with 'CorSSL v1.1.1t.001'. See: https://github.com/urllib3/urllib3/issues/3020
+./3-run.sh 2>/dev/null
 cd ../
 
