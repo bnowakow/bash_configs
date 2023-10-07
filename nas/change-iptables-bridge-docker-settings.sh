38747df6215017f5f4fa32dabc25ec7a6a0a44e1
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2023-10-07 06:42:57 -0700

    upgrade elk stack. change iptables docker bridge script

--- nas/change-iptables-bridge-docker-settings.sh
+++ nas/change-iptables-bridge-docker-settings.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-sed -i "s/,\ \"bridge\"[^}]*//" /etc/docker/daemon.json;
-sed -i "s/,\ \"iptables\"[^}]*//" /etc/docker/daemon.json; 
-service docker restart;
+# TODO fix regexp to only remove two config variables
+# TODO figure out how to add it so it starts at TrueNAS boot
+#sed -i "s/,\ \"bridge\"[^}]*//" /etc/docker/daemon.json;
+#sed -i "s/,\ \"iptables\"[^}]*//" /etc/docker/daemon.json; 
+# old
+#service docker restart;
+# TODO FIXME workaround for above
+cp /mnt/MargokPool/home/sup/code/bash_configs/nas/daemon.json.CHANGED /etc/docker/daemon.json
+
+# new
+service docker restart; service k3s restart; service docker restart; 
+service docker status; service k3s status
+
+# when k3s doesn't start: https://github.com/kubernetes/kubernetes/issues/66241#issuecomment-460832038
 
