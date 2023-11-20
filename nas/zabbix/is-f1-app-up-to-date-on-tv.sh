048cd3f3cb09adafd3c7119633cece13594d761d
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2023-11-20 08:33:32 -0800

    ELK upgrade. is-f1-app-up-to-date-on-tv.sh and is-pihole-returning-dns-queries.sh WIP

--- nas/zabbix/is-f1-app-up-to-date-on-tv.sh
+++ nas/zabbix/is-f1-app-up-to-date-on-tv.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+directory_with_apks="/mnt/MargokPool/archive/Software/AndroidTV/f1"
+
 current_version=$(curl https://www.androidout.com/item/android-apps/1002931/f1-tv/download/ 2>/dev/null| grep 'Version' | head -n1 | sed 's/.*Version[^0-9]*//' | sed 's/<\/div>.*//')
 
 echo $current_version;
+ls $directory_with_apks
+
---
+++ nas/zabbix/is-pihole-returning-dns-queries.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dig @192.168.0.20 google.com
+
