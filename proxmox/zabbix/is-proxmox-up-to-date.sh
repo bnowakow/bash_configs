6dcfce7d1ac316c594b2e9af5720be26178a28cb
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2024-01-31 20:40:11 +0100

    modify is-proxmox-up-to-date.sh to check only minor and minor version without build one

--- proxmox/zabbix/is-proxmox-up-to-date.sh
+++ proxmox/zabbix/is-proxmox-up-to-date.sh
@@ -3,8 +3,8 @@
 # https://subscription.packtpub.com/book/cloud-&-networking/9781783980901/11/ch11lvl1sec94/proxmox-commands
 # https://www.youtube.com/watch?v=NHAUugksnvA
 
-local_version=$(pveversion -v | grep proxmox-ve | sed 's/proxmox-ve: //' | sed 's/ .running.*'//)
-current_version=$(curl https://www.proxmox.com/en/downloads/proxmox-virtual-environment/iso 2>/dev/null| grep '\.iso' | head -1 | sed 's/.*proxmox-ve_//' | sed 's/.iso.*//' | sed 's/-/\./')
+local_version=$(pveversion -v | grep proxmox-ve | sed 's/proxmox-ve: //' | sed 's/ .running.*//' | sed 's/\.[0-9]*$//')
+current_version=$(curl https://www.proxmox.com/en/downloads/proxmox-virtual-environment/iso 2>/dev/null| grep '\.iso' | head -1 | sed 's/.*proxmox-ve_//' | sed 's/.iso.*//' | sed 's/-[0-9]*//')
 
 if [ "$local_version" = "$current_version" ]; then
     echo true
