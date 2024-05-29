9e15515047ba6397e308d97fb74a8df37775748a
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2024-05-29 22:40:44 +0200

    try two paths for zabbix.conf in update-zabbix-metadata.sh

--- zabbix/update-zabbix-metadata.sh
+++ zabbix/update-zabbix-metadata.sh
@@ -9,6 +9,7 @@ fi
 cd /etc/zabbix/zabbix_agent2.d/bash_configs/zabbix
 
 cp zabbix_agent2.conf ../../../
+cp zabbix_agent2.conf /etc/zabbix/zabbix_agent2.conf
 rm /etc/zabbix/zabbix_agent2.d/zabbix*.conf
 find -L /etc/zabbix/zabbix_agent2.d -name 'zabbix*.conf' | grep -v zabbix_agent2.conf | xargs -I{} cp {} /etc/zabbix/zabbix_agent2.d/
 service zabbix-agent2 restart
