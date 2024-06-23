656fe0ffc25fc73abe28a9943ae769d465983c63
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2024-06-23 16:40:55 +0200

    add crontab to rancher

---
+++ rancher/cron/git-pull.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# possible conflicts while pulling: crashplan, docker-mailserver (docker-compose, Makefile), homer (config), medihunter, zabbix-scripts (.gitignore)
+
+dirs=("/home/sup/code/bash_configs/repos/truecharts" \
+)
+
+for dir in ${dirs[@]}; do
+    echo $dir
+    cd $dir
+    if [ -f "git-pull.sh" ]; then
+        ./git-pull.sh;
+    else 
+        git pull
+    fi
+    git status | grep branch
+    # TODO check if there was a merge conflict
+    echo;
+done
+
