aa3ef06beef8291760127779ba3793aab9b65099
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2024-11-03 17:18:32 +0100

    remove_jpg_if_raw_exists.sh

--- taerar/remove_jpg_if_raw_exists.sh
+++ taerar/remove_jpg_if_raw_exists.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -x
 
-sd_card_path="/Volumes/sup-om2-JPG/DCIM/100OLYMP"
+sd_card_path="/Volumes/sup-om2-JPG/DCIM/106OLYMP"
+
 if cd $sd_card_path; then
-    find . | tr './' ' '  | awk '{print $1}' | xargs -I {} find ~/Pictures -name "*{}*" | sed 's/.*\///g' | tr 'ORF' 'JPG' | xargs -I {} rm {}
-    find . | tr './' ' '  | awk '{print $1}' | xargs -I {} find /Volumes/2024-Lightroom-private/2024-Lightroom-private -name "*{}*" | sed 's/.*\///g' | tr 'ORF' 'JPG' | xargs -I {} rm {}
+#    find . | tr './' ' '  | awk '{print $1}' | xargs -I {} find ~/Pictures/2024-rolki -name "*{}*" | grep ORF | sed 's/.*\///g' | tr 'ORF' 'JPG' | xargs -I {} rm {}
+#    find . | tr './' ' '  | awk '{print $1}' | xargs -I {} find /Volumes/2024-Lightroom-private -name "*{}*" | sed 's/.*\///g' | tr 'ORF' 'JPG' | xargs -I {} rm {}
+    find . | tr './' ' '  | awk '{print $1}' | xargs -I {} find ~/Pictures/2024-Lightroom -name "*{}*" | sed 's/.*\///g' | tr 'ORF' 'JPG' | xargs -I {} rm {}
 
     # TODO fix repetition (do foreach)
     #cd /Volumes/sup-om2-JPG/DCIM/101OLYMP;
