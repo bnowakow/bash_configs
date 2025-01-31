f19165b0883a5453515a985a8e659acc0b7637ad
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2025-01-31 17:24:30 +0100

    update is-apt-up-to-date.sh for The following packages have been kept back

--- ovh/zabbix/is-apt-up-to-date.sh
+++ ovh/zabbix/is-apt-up-to-date.sh
@@ -5,9 +5,9 @@ export LC_ALL=en_US.UTF-8
 
 # https://askubuntu.com/a/510161
 sudo /bin/apt-get update > /dev/null 2> /dev/null
-if sudo /bin/apt-get --simulate upgrade | grep "0 upgraded" > /dev/null; then
+if ! sudo /bin/apt-get --simulate upgrade | grep 'The following packages will be upgraded' > /dev/null && ! sudo /bin/apt-get --simulate upgrade | grep 'The following packages have been kept back' > /dev/null; then
     echo true
 else
-    echo false,$(sudo /bin/apt-get --simulate upgrade | grep 'The following packages will be upgraded' -A1 | tail -1)
+    echo false,$(sudo /bin/apt-get --simulate upgrade | grep 'The following packages will be upgraded' -A1 | tail -1) $(sudo /bin/apt-get --simulate upgrade | grep 'The following packages have been kept back' -A1 | tail -1)
 fi
 
