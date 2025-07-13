cce60ef8d7c09e5eaeabea97a7e9f8aa88f9bced
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2025-07-13 18:15:13 +0200

    fixes to fix-config.sh

--- transmission/fix-config.sh
+++ transmission/fix-config.sh
@@ -8,4 +8,7 @@ cp /mnt/MargokPool/home/sup/code/bash_configs/transmission/settings.json /mnt/Pl
 chown debian-transmission:debian-transmission /mnt/PlexPool/plex/transmission-daemon/settings.json
 service transmission-daemon start
 service transmission-daemon status
+echo
+diff /mnt/MargokPool/home/sup/code/bash_configs/transmission/settings.json /mnt/PlexPool/plex/transmission-daemon/settings.json
+echo "if diff shows something then check if ExecStart in /lib/systemd/system/transmission-daemon.service doesn't override it"
 
