539e8900aee8ffb85879e58a82a7811ddc57658e
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2025-07-13 17:20:15 +0200

    update fix-config.sh

--- transmission/fix-config.sh
+++ transmission/fix-config.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# TODO make sure running as root
+
 service transmission-daemon stop
-#su debian-transmission -c "cp /mnt/MargokPool/home/sup/code/nordvpn-torrent/settings.json /mnt/PlexPool/plex/transmission-daemon/settings.json"
-cp /mnt/MargokPool/home/sup/code/nordvpn-torrent/settings.json /mnt/PlexPool/plex/transmission-daemon/settings.json
+# cp /mnt/MargokPool/home/sup/code/nordvpn-torrent/settings.json /mnt/PlexPool/plex/transmission-daemon/settings.json
+cp /mnt/MargokPool/home/sup/code/bash_configs/transmission/settings.json /mnt/PlexPool/plex/transmission-daemon/settings.json
+chown debian-transmission:debian-transmission /mnt/PlexPool/plex/transmission-daemon/settings.json
 service transmission-daemon start
+service transmission-daemon status
 
