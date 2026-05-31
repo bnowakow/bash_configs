48f2b2df7a6a2e853342421dd7efe3abf7f5f21c
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2026-05-31 21:07:19 +0200

    update sie-pomaga scripts to new json

--- nas/zabbix/sie-pomaga/sie-pomaga-figlet-show-last-value.sh
+++ nas/zabbix/sie-pomaga/sie-pomaga-figlet-show-last-value.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+. "$script_dir/sie-pomaga-lib.sh"
+
 font="Doh.flf"
+
+sie_pomaga_cd
 
 while true; do
 
-    amount=$(jq .data.amount sie-pomaga-value-last.json)
-    payments_count=$(jq .data.payments_count sie-pomaga-value-last.json)
+    amount=$(jq .data.funds_current sie-pomaga-value-last.json)
+    donors_count=$(jq .data.donors_count sie-pomaga-value-last.json)
 
     # fonts are from https://github.com/xero/figlet-fonts
 
@@ -13,7 +18,6 @@ while true; do
     date -r sie-pomaga-value-last.json | /usr/bin/figlet-figlet -w1000 -f "Nancyj.flf";
     # https://unix.stackexchange.com/a/113798
     echo $amount PLN | sed ':a;s/\B[0-9]\{3\}\>/\ \ &/;ta' | /usr/bin/figlet-figlet -w1000 -f $font
-    echo $payments_count osob | sed ':a;s/\B[0-9]\{3\}\>/\ \ &/;ta' | /usr/bin/figlet-figlet -w1000 -f $font
+    echo $donors_count osob | sed ':a;s/\B[0-9]\{3\}\>/\ \ &/;ta' | /usr/bin/figlet-figlet -w1000 -f $font
     sleep 30;
 done
-
