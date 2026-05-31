f77fa67e2fcafaa36483e6300f6674f4898ac56f
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2026-05-31 21:20:52 +0200

    fixes for debian 13 in proxmox_backup_download.sh

--- nas/cron/proxmox_backup_download.sh
+++ nas/cron/proxmox_backup_download.sh
@@ -2,9 +2,30 @@
 
 remote_user="root"
 
+green='\033[0;32m'
+red='\033[0;31m'
+yellow='\033[1;33m'
+no_color='\033[0m'
+
+success() {
+    printf "%b\n" "${green}$*${no_color}"
+}
+
+error() {
+    printf "%b\n" "${red}$*${no_color}"
+}
+
+warning() {
+    printf "%b\n" "${yellow}$*${no_color}"
+}
+
 remote_hosts=( proxmox2.localdomain.bnowakowski.pl proxmox3.localdomain.bnowakowski.pl proxmox4.localdomain.bnowakowski.pl );
+remote_hosts=( proxmox2-old.localdomain.bnowakowski.pl proxmox4.localdomain.bnowakowski.pl );
+
+# DEBUG
+#remote_hosts=( proxmox4.localdomain.bnowakowski.pl );
 for remote_host in "${remote_hosts[@]}"; do
-    echo $remote_host
+    warning "backing up $remote_host"
 local_dir="/mnt/MargokPool/archive/Backups/proxmox/config"
 current_backup_local_dir="$local_dir/current"
 
@@ -22,11 +43,11 @@ remote_path="$remote_dir/$remote_file_name";
 rsync --partial --progress --rsh=ssh -r $remote_user@$remote_host:$remote_path $current_backup_local_dir/qemu-server/;
 number_of_copied_files=$(find $current_backup_local_dir/qemu-server -type f -name "$remote_file_name" | wc -l)
 if [ $number_of_copied_files = 0 ]; then
-    echo copy of $remote_dir on $remote_host failed
+    error "copy of $remote_dir on $remote_host failed"
     rm -rf $current_backup_local_dir
     exit
 else
-    echo copied $number_of_copied_files files from remote_dir
+    success "copied $number_of_copied_files files from $remote_dir"
 fi
 
 # modprobe.d
@@ -35,11 +56,11 @@ remote_path="$remote_dir/$remote_file_name";
 rsync --partial --progress --rsh=ssh -r $remote_user@$remote_host:$remote_path $current_backup_local_dir/modprobe.d/;
 number_of_copied_files=$(find $current_backup_local_dir/modprobe.d -type f | wc -l)
 if [ $number_of_copied_files = 0 ]; then
-    echo copy of $remote_dir on $remote_host failed
+    error "copy of $remote_dir on $remote_host failed"
     rm -rf $current_backup_local_dir
     exit
 else
-    echo copied $number_of_copied_files files from $remote_dir
+    success "copied $number_of_copied_files files from $remote_dir"
 fi
 
 #remote_dir="/var/lib/vz/snippets"
@@ -60,11 +81,11 @@ remote_dir="/etc/default/grub"
 rsync --partial --progress --rsh=ssh -r $remote_user@$remote_host:$remote_dir $current_backup_local_dir/;
 number_of_copied_files=$(find $current_backup_local_dir/grub -type f | wc -l)
 if [ $number_of_copied_files = 0 ]; then
-    echo copy of $remote_dir on $remote_host failed
+    error "copy of $remote_dir on $remote_host failed"
     rm -rf $current_backup_local_dir
     exit
 else
-    echo copied $number_of_copied_files files from $remote_dir
+    success "copied $number_of_copied_files files from $remote_dir"
 fi
 
 
@@ -72,36 +93,30 @@ remote_dir="/etc/modules"
 rsync --partial --progress --rsh=ssh -r $remote_user@$remote_host:$remote_dir $current_backup_local_dir/;
 number_of_copied_files=$(find $current_backup_local_dir/modules -type f | wc -l)
 if [ $number_of_copied_files = 0 ]; then
-    echo copy of $remote_dir on $remote_host failed
+    error "copy of $remote_dir on $remote_host failed"
     rm -rf $current_backup_local_dir
     exit
 else
-    echo copied $number_of_copied_files files from $remote_dir
+    success "copied $number_of_copied_files files from $remote_dir"
 fi
 
 remote_dir="/etc/apt/sources.list"
-rsync --partial --progress --rsh=ssh -r $remote_user@$remote_host:$remote_dir $current_backup_local_dir/;
+rsync --partial --progress --rsh=ssh -r $remote_user@$remote_host:$remote_dir $current_backup_local_dir/ 2>/dev/null || warning "optional $remote_dir was not copied from $remote_host"
 remote_dir="/etc/apt/sources.list.bak"
-rsync --partial --progress --rsh=ssh -r $remote_user@$remote_host:$remote_dir $current_backup_local_dir/;
-number_of_copied_files=$(find "$current_backup_local_dir/sources.list" -type f | wc -l)
-number_of_copied_files_bak=$(find "$current_backup_local_dir/sources.list.bak" -type f | wc -l)
-if [ "$number_of_copied_files" = 0 ] && [ "$number_of_copied_files_bak" = 0 ]; then
-    echo copy of $remote_dir on $remote_host failed
-    rm -rf $current_backup_local_dir
-    exit
-else
-    echo copied $number_of_copied_files files from $remote_dir
-fi
-
+rsync --partial --progress --rsh=ssh -r $remote_user@$remote_host:$remote_dir $current_backup_local_dir/ 2>/dev/null || warning "optional $remote_dir was not copied from $remote_host"
 remote_dir="/etc/apt/sources.list.d"
 rsync --partial --progress --rsh=ssh -r $remote_user@$remote_host:$remote_dir $current_backup_local_dir/;
-number_of_copied_files=$(find "$current_backup_local_dir/sources.list.d" -type f | wc -l)
-if [ $number_of_copied_files = 0 ]; then
-    echo copy of $remote_dir on $remote_host failed
+
+number_of_copied_sources_list=$(find "$current_backup_local_dir/sources.list" -type f 2>/dev/null | wc -l)
+number_of_copied_sources_list_bak=$(find "$current_backup_local_dir/sources.list.bak" -type f 2>/dev/null | wc -l)
+number_of_copied_sources_list_d=$(find "$current_backup_local_dir/sources.list.d" -type f \( -name "*.list" -o -name "*.sources" \) 2>/dev/null | wc -l)
+number_of_copied_apt_sources=$((number_of_copied_sources_list + number_of_copied_sources_list_bak + number_of_copied_sources_list_d))
+if [ "$number_of_copied_apt_sources" = 0 ]; then
+    error "copy of apt sources on $remote_host failed"
     rm -rf $current_backup_local_dir
     exit
 else
-    echo copied $number_of_copied_files files from $remote_dir
+    success "copied $number_of_copied_apt_sources apt source files"
 fi
 
 #remote_dir="/etc/sudoers"
